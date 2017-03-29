5b7b9a4cb2156f89e605093983de36da408832e5
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-03-29 23:50:12 +1300

    Add some tests for home.js

--- test/Home.js
+++ test/Home.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { createRenderer } from 'react-addons-test-utils';
-import createComponent from 'react-unit';
-import test from 'tape';
-import addAssertions from 'extend-tape';
-import jsxEquals from 'tape-jsx-equals';
+import React from 'react'
+import { createRenderer } from 'react-addons-test-utils'
+import createComponent from 'react-unit'
+import tape from 'tape'
+import addAssertions from 'extend-tape'
+import jsxEquals from 'tape-jsx-equals'
+const test = addAssertions(tape, {jsxEquals})
+
+import Home from '../components/Home'
+
+const testState = {
+  name: 'NBA Battle',
+  currentPage: 'index',
+  players: [],
+  form: {
+    name: null,
+    suggestions: []
+  },
+  isLoading: false
+}
 
 test('Base test for Home.js should pass', t => {
   t.pass('passes for home.js')
+  t.end()
+})
+
+test('React Components Test: Home.js', t => {
+  const component = createComponent.shallow(<Home />)
+  t.true(component.props.state)
+  t.true(component.props.dispatch)
   t.end()
 })
