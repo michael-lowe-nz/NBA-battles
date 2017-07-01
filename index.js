bcfa3c84e49e0e299dd56aafb7838f99df8473f2
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-07-01 18:05:00 +1200

    Updates

--- index.js
+++ index.js
@@ -9,6 +9,62 @@ import reducer from './reducer'
 var initialState = {
   name: 'NBA Battle',
   currentPage: 'index',
+  columns: [
+    {
+      columnName: "Player",
+      statName: "fullName",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "PPG",
+      statName: "pts",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "APG",
+      statName: "ast",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "RPG",
+      statName: "reb",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "FG%",
+      statName: "fgPct",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "3P%",
+      statName: "fg3Pct",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "FT%",
+      statName: "ftPct",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "STL",
+      statName: "stl",
+      sorted: false,
+      isDescending: false,
+    },
+    {
+      columnName: "BLK",
+      statName: "blk",
+      sorted: false,
+      isDescending: false,
+    },
+  ],
   players: [],
   form: {
     name: "",
